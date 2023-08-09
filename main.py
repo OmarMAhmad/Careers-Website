9870fb4951d629c4108c3f4b2994976936dc5792
Omar M Ahmad <omarmahmodahmad63@gmail.com>
2023-08-09 13:00:46 +0300

    added daynamic data and API route

--- main.py
+++ main.py
@@ -1,11 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
 
+JOBS = [
+    {
+        "id": 1,
+        "title": "Data Analyst",
+        "location": "Bengaluru, India",
+        "salary": "Rs. 1,000,000",
+    },
+    {
+        "id": 2,
+        "title": "Data Scientist",
+        "location": "Delhi, India",
+        "salary": "Rs. 1,500,000",
+    },
+    {
+        "id": 3,
+        "title": "Frount Engineer",
+        "location": "Remote",
+        "salary": "Rs. 1,200,000",
+    },
+    {
+        "id": 4,
+        "title": "Backend Engineer",
+        "location": "San Francisco, USA",
+        "salary": "$120,000",
+    },
+]
+
+
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", jobs=JOBS)
+
+
+@app.route("/api/jobs")
+def list_jobs():
+    return jsonify(JOBS)
 
 
 if __name__ == "__main__":
